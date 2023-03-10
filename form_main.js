79bdbfe220a499698ac3a91e660d00c91402d080
Emils <emilsberzs@hotmail.com>
2023-03-10 12:24:13 +0000

    Exercise complete, fomr validation works on all fields including checking if password and password confirm match

--- form_main.js
+++ form_main.js
@@ -12,8 +12,8 @@ const zip_error = document.querySelector('#zip + span.error');
 const password = document.getElementById('password');
 const password_error = document.querySelector('#password + span.error')
 
-const confirm = document.getElementById('confirm')
-const confirm_error = document.querySelector('#confirm + span.error')
+const confirmation = document.getElementById('confirm')
+const confirmation_error = document.querySelector('#confirm + span.error')
 
 
 email.addEventListener('input', (e) => {
@@ -23,18 +23,70 @@ email.addEventListener('input', (e) => {
     } else {
         showError(email, email_error)
     }
+});
+
+country.addEventListener('input', (e) => {
+    if (country.validity.valid) {
+        country_error.textContent = '';
+        country_error.className = 'error'
+    } else {
+        showError(country, country_error);
+    }
+});
+
+zip.addEventListener('input', (e) => {
+    if (zip.validity.valid) {
+        zip_error.textContent = ''
+        zip_error.className = 'error'
+    } else {
+        showError(zip, zip_error)
+    }
+});
+
+password.addEventListener('input', (e) => {
+    if (password.validity.valid) {
+        password_error.textContent = '';
+        password_error.className = 'error';
+    } else {
+        showError(password, password_error);
+    }
+})
+
+confirmation.addEventListener('input', (e) => {
+    if (confirmation.validity.valid) {
+        confirmation_error.textContent = '';
+        confirmation_error.className = 'error';
+    } else {
+        showError(confirmation, confirmation_error);
+    }
 })
 
 form.addEventListener('submit', (e) => {
     if (!email.validity.valid) {
         showError(email, email_error);
+        e.preventDefault();
+    } else if (!country.validity.valid) {
+        showError(country, country_error);
+        e.preventDefault();
+    } else if (!zip.validity.valid) {
+        showError(zip, zip_error)
         e.preventDefault()
+    } else if (!password.validity.valid) {
+        showError(password, password_error);
+        e.preventDefault();
+    } else if (!confirmation.validity.valid) {
+        showError(confirmation, confirmation_error);
+        e.preventDefault()
+    } else if (password.value != confirmation.value) {
+        password_error.textContent = "Passwords do not match, please try again";
+        e.preventDefault();
     }
-})
+});
+
 
 function showError(element, error) {
     if (element.validity.valueMissing) {
-        error.textContent = 'Please Enter Valid Email Address';
+        error.textContent = `Please Enter Valid ${element.name}`;
     } else if (element.validity.typeMismatch) {
         error.textContent = 'Entered value does not look right';
     } else if (element.validity.tooShort) {
@@ -43,3 +95,5 @@ function showError(element, error) {
          g You need ${element.minLength - element.value.length} more characters`
     }
 }
+
+console.log(confirmation.innerHTML)
